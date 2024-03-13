4a4aa1d88a1c34844e0f9a3492c2655dc620a415
jparkley <parkleydondon@yahoo.com>
2024-03-12 21:11:39 -0700

    remove v1 code

--- src/components/checkout/CartModal.tsx
+++ src/components/checkout/CartModal.tsx
@@ -1,26 +1,15 @@
 'use client';
 
-import { useCart } from "@/context/CartContext";
 import { useState } from "react";
-import Modal from "react-modal";
+import { useCart } from "@/context/CartContext";
 import CartItem from "./CartItem";
-// import { useShoppingCart } from "use-shopping-cart"
-// import useCheckout from "../utils/useCheckout"
-// import CartItem from "./CartItem"
-
-// Modal.setAppElement("#root")
-Modal.setAppElement('body');
 
 const CartModal = ({ isOpen = true, toggleModal = true }) => {
-  // const handleCheckout = useCheckout()
-  // const { cartCount, formattedTotalPrice, cartDetails } = useShoppingCart()
-  // const cartItems = Object.keys(cartDetails).map(key => cartDetails[key])
 
   const [showModal, setShowModal] = useState(isOpen);
 
    const { cart: cartItems } = useCart();
   console.log('==== cart in modal', cartItems)
-  console.log('==== isopen', isOpen)
 
   return (
     <>
@@ -29,11 +18,7 @@ const CartModal = ({ isOpen = true, toggleModal = true }) => {
         type="button"
         onClick={() => setShowModal(true)}
       >Cart</button>
-      {/* // <Modal isOpen={isOpen} onRequestClose={toggleModal} contentLabel="Cart Modal" closeTimeoutMS={500} className="cart-modal" overlayClassName="cart-overlay"> */}
       {showModal && 
-      // <Modal isOpen={isOpen} contentLabel="Cart Modal" closeTimeoutMS={500} className="cart-modal" overlayClassName="cart-overlay"
-      //   // ariaHideApp={false}
-      // >
         <div className="fixed inset-0 flex flex-col justify-center items-center bg-gray-900 bg-opacity-50 z-50">
           <div className="relative inline-flex justify-between">
             <div className="relative flex flex-col bg-orange-50 py-10 px-8">
@@ -42,9 +27,15 @@ const CartModal = ({ isOpen = true, toggleModal = true }) => {
               </div>            
 
               <div className="modal-body">
-                {cartItems && cartItems.map(item => (
-                  <CartItem key={item.id} item={item} />
-                ))}
+                { cartItems.length == 0 ? (
+                  <div className="flex justify-center pt-8">
+                    <p>Your cart is empty</p>
+                  </div>
+                ) : (
+                  cartItems.map(item => (
+                    <CartItem key={item.id} item={item} />
+                  ))
+                )}
               </div>
 
               <span className="d-flex align-items-center">
@@ -53,8 +44,7 @@ const CartModal = ({ isOpen = true, toggleModal = true }) => {
                     {/* {formattedTotalPrice} ({cartCount} Items) */}
                   </strong>
                   {/* <button type="button" className="btn-close ms-1" aria-label="Close" onClick={toggleModal}></button> */}
-                </span>
-
+              </span>
 
               <div className="flex justify-end mt-6">
                 {/* <button type="button" className="btn btn-primary text-dark" onClick={handleCheckout}>
@@ -66,14 +56,11 @@ const CartModal = ({ isOpen = true, toggleModal = true }) => {
                   data-bs-dismiss="modal" 
                   // onClick={toggleModal}
                   onClick={() => setShowModal(false)}
-                >
-                X
-                </button>
+                > X </button>
               </div>
             </div>
           </div>
         </div>
-      // </Modal>
       }
     </>
   )
